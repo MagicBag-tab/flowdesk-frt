7174ce519d64e7684554e975e00227ec22d592a8
Sarah <sarahestrada33@gmail.com>
2026-05-27 03:19:45 -0600

    arreglando problema de deploy

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -6,20 +6,29 @@ export const handler: Handler = async (event) => {
   const path = event.path.replace('/.netlify/functions/proxy', '');
   const url = `${API_BASE}${path}`;
 
-  const response = await fetch(url, {
-    method: event.httpMethod,
-    headers: {
-      'Content-Type': 'application/json',
-      ...(event.headers.authorization ? { Authorization: event.headers.authorization } : {}),
-    },
-    body: event.body ?? undefined,
-  });
+  try {
+    const response = await fetch(url, {
+      method: event.httpMethod,
+      headers: {
+        'Content-Type': 'application/json',
+        ...(event.headers.authorization
+          ? { Authorization: event.headers.authorization }
+          : {}),
+      },
+      body: event.body ?? undefined,
+    });
 
-  const data = await response.text();
+    const data = await response.text();
 
-  return {
-    statusCode: response.status,
-    headers: { 'Content-Type': 'application/json' },
-    body: data,
-  };
+    return {
+      statusCode: response.status,
+      headers: { 'Content-Type': 'application/json' },
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 502,
+      body: JSON.stringify({ detail: 'Proxy error' }),
+    };
+  }
 };
