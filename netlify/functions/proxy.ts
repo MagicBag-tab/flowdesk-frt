8982b6d7dd89caaad2685e0a2cda221a9a6e043b
MagicBag-tab <est24347@uvg.edu.gt>
2026-08-11 20:29:42 -0600

    fix: deployment

--- netlify/functions/proxy.ts
+++ netlify/functions/proxy.ts
@@ -1,15 +1,21 @@
 import type { Handler } from '@netlify/functions';
 
-const API_BASE = (process.env.API_BASE_URL ?? process.env.VITE_API_BASE_URL ?? 'http://98.92.232.157').replace(/\/+$/, '');
+const API_BASE = (process.env.API_BASE_URL ?? 'http://98.92.232.157').replace(/\/+$/, '');
+const REQUEST_TIMEOUT_MS = 25000;
 
 export const handler: Handler = async (event) => {
   const path = event.path.replace('/.netlify/functions/proxy', '');
   const url = `${API_BASE}${path}`;
-
   const authHeader = event.headers.authorization || event.headers.Authorization;
+
+  console.log('proxy request', { apiBase: API_BASE, path, url, method: event.httpMethod });
+
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), REQUEST_TIMEOUT_MS);
 
   try {
     const response = await fetch(url, {
+      signal: controller.signal,
       method: event.httpMethod,
       headers: {
         'Content-Type': 'application/json',
